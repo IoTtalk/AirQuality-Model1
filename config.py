95bf2f3195e4b56de0fb6e0befc5ab5fbe3d086a
Jyneda <jyneda@gmail.com>
2024-09-23 23:43:33 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,23 @@
+ServerURL='http://IP:9999'   
+dm_name = '?'
+d_name = None
+
+MQTT_broker =  None
+MQTT_port = 1883
+MQTT_User = '?'
+MQTT_PW = '?'
+
+Comm_interval = 1 # unit:second
+
+idf_list = [
+idf_list = [
+    ('AtPressure', float),
+    ('CO2', float),
+    ('Gas', float),
+    ('Humidity', float),
+    ('Temperature', float),
+    ]
+
+
+
+
